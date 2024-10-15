4ca24aa4831ff0a152fbb6eeb14f1964aadd77cc
Amin Mortezaie <mohammadmortezaie@gmail.com>
2024-10-15 16:21:22 +0330

    feat: add constructor and reqs.

--- src/UniswapV3Pool.sol
+++ src/UniswapV3Pool.sol
@@ -1,8 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.14;
 
-contract UniswapV3Pool {
-    constructor(){
+import "lib/Tick.sol";
+import "lib/Position.sol";
 
+contract UniswapV3Pool {
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes => Position.Info);
+    using Position for Position.Info;
+
+    int24 internal constant MIN_TICK = - 887272;
+    int24 internal constant MAX_TICK = - MIN_TICK;
+
+    address public immutable token0;
+    address public immutable token1;
+
+    struct Slot0 {
+        uint160 sqrtPriceX96;
+        int24 tick;
     }
+
+    Slot0 public slot0;
+    uint160 public liquidity;
+
+    mapping(int24 => Tick.Info) public ticks;
+    mapping(bytes => Position.Info) public positions;
+
+    constructor(
+        address token0_,
+        address token1_,
+        uint160 sqrtPriceX96,
+        int24 tick
+    ){
+        token0 = token0_;
+        token1 = token1_;
+
+        slot0 = Slot0({sqrtPriceX96: sqrtPriceX96, tick: tick});
+    }
+
+
+
+
 }
